38e7d82f9acb39485284432f0e502e2a9f22006b
beabritoliveira <103044603+beabritoliveira@users.noreply.github.com>
2023-05-22 10:00:32 -0300

    Add files via upload

---
+++ UsandoNavigation/src/pages/LoginPage/style.js
@@ -0,0 +1,14 @@
+import { StyleSheet } from "react-native";
+
+const estilo = StyleSheet.create({
+    container:{
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    texto:{
+        fontSize: 30
+    }
+});
+
+export default estilo;
